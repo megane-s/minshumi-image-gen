923c8d262950a95955697ecdedd96b6c94aff0d7
TBSten <programmingcafeteria@gmail.com>
2023-11-29 14:37:50 +0900

    paddingを追加

--- image/layout.py
+++ image/layout.py
@@ -49,6 +49,68 @@ class Size:
         return Size(w, h)
 
 
+class Padding:
+    def __init__(
+        self,
+        left: Length,
+        top: Length,
+        right: Length,
+        bottom: Length,
+    ) -> None:
+        self._left = left
+        self._top = top
+        self._right = right
+        self._bottom = bottom
+
+    @property
+    def left(self):
+        return int(self._left)
+
+    @property
+    def top(self):
+        return int(self._top)
+
+    @property
+    def right(self):
+        return int(self._right)
+
+    @property
+    def bottom(self):
+        return int(self._bottom)
+
+    @staticmethod
+    def new_all(padding: Length):
+        return Padding(
+            padding,
+            padding,
+            padding,
+            padding,
+        )
+
+    @staticmethod
+    def new_vh(vertical: Length = 0, horizontal: Length = 0):
+        return Padding(
+            top=vertical,
+            bottom=vertical,
+            left=horizontal,
+            right=horizontal,
+        )
+
+    @staticmethod
+    def new_ltrb(
+        left: Length,
+        top: Length,
+        right: Length,
+        bottom: Length,
+    ):
+        return Padding(
+            left=left,
+            top=top,
+            right=right,
+            bottom=bottom,
+        )
+
+
 class Rect:
     def __init__(self, offset: Offset, size: Size) -> None:
         self._offset = offset
@@ -67,24 +129,48 @@ class Rect:
         return self.offset.x
 
     @property
+    def left(self) -> int:
+        return self.l
+
+    @property
     def t(self) -> int:
         return self.offset.y
+
+    @property
+    def top(self) -> int:
+        return self.t
 
     @property
     def r(self) -> int:
         return int(self.offset._x + self.size._w)
 
     @property
+    def right(self) -> int:
+        return self.r
+
+    @property
     def b(self) -> int:
         return int(self.offset._y + self.size._h)
+
+    @property
+    def bottom(self) -> int:
+        return self.b
 
     @property
     def w(self) -> int:
         return self.offset.w
 
     @property
+    def width(self) -> int:
+        return self.w
+
+    @property
     def h(self) -> int:
         return self.offset.h
+
+    @property
+    def height(self) -> int:
+        return self.h
 
     @property
     def ltrb(self):
@@ -101,6 +187,14 @@ class Rect:
 
     def __str__(self) -> str:
         return f"Rect(t={self.t},l={self.l},r={self.r},b={self.b})"
+
+    def inner_rect(self, padding=Padding.new_all(0)):
+        return Rect.new_ltwh(
+            left=self.l + padding.left,
+            top=self.t + padding.top,
+            w=self.w - padding.left - padding.right,
+            h=self.h - padding.top - padding.bottom,
+        )
 
     @staticmethod
     def new_ltrb(left: Length, top: Length, right: Length, bottom: Length):
