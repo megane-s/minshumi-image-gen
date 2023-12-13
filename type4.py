8c8d2abc6cdd7d3d075c764ca0a1a8b7f756e99d
nagao-meganes <nagao.meganes@gmail.com>
2023-12-13 14:53:48 +0900

    type4を実装

--- type4.py
+++ type4.py
@@ -1,50 +1,107 @@
 from PIL import Image, ImageDraw
-from image.cut import  cut_rounded_rect
+from image.cut import cut_circle, cut_rounded_rect
 from image.flow_layout import draw_text_with_wrap, textbbox_with_wrap
 from settings import get_font
-from image.layout import  Rect
+from image.layout import Rect
 from colors import colors
 
 
-NAME_FONT = get_font(60)
-ART_FONT = get_font(35)
+NAME_FONT = get_font(90)
+ART_FONT = get_font(40)
 
-def _draw_background_filter(img: Image.Image):
-    overlay_img = Image.new("RGBA", img.size)
-    overlay_draw = ImageDraw.Draw(overlay_img)
-    overlay_draw.rectangle(
-        (0, 0, 0 + img.width, 0 + img.height), fill=(0, 0, 0, int(255 * 0.3))
+
+def _draw_icon_and_name(
+    img: Image.Image,
+    icon,
+    username,
+    colors,
+):
+    draw = ImageDraw.Draw(img)
+
+    # サイズ計算
+
+    # アイコンのサイズ
+    icon_w, icon_h = (260, 260)
+
+    # 名前のサイズ
+    _, _, name_w, name_h = textbbox_with_wrap(
+        draw=draw,
+        xy=(0, 0),
+        width=img.width,
+        text=username,
+        font=NAME_FONT,
+        stroke_width=5,
     )
-    img.alpha_composite(overlay_img)
+
+    # 位置計算
+    icon_x = img.width / 2 - icon_w / 2
+    icon_y = 70
+    name_x = img.width / 2 - name_w / 2
+    name_y = img.height - 70 - icon_h - 10
+
+    # 描画
+    # アイコンの描画
+    icon_img = Image.open(icon)
+    icon_img = cut_circle(icon_img.resize((icon_w, icon_h)))
+    img.alpha_composite(icon_img, dest=(int(icon_x), int(icon_y)))
+
+    # 名前の描画
+    draw_text_with_wrap(
+        draw=draw,
+        xy=(name_x, name_y),
+        width=img.width,
+        text=username,
+        font=NAME_FONT,
+        stroke_width=5,
+        fill=colors.text.inner,
+        stroke_fill=colors.text.edge,
+    )
+
+    pass
+
 
 def get_arts_rect(
     img: Image.Image,
 ):
-    art_h = 170
+    art_h = 157
     return Rect.new_ltwh(
-        left=47,
-        top=img.height - 34 - art_h,
-        w=img.width - 47 * 2,
+        left=40,
+        top=img.height - 38 - art_h,
+        w=img.width - 40 * 2,
         h=art_h,
     )
+
 
 def _draw_arts(
     img: Image.Image,
     arts,
-    colors,
+    color,
 ):
-    art_w = (img.width - 47 - 47 - (20 * 2)) / 3
+    draw = ImageDraw.Draw(img)
+
+    # サイズ計算
+    art_width, art_height = (350, 157)
+    arts_h_gap = 35  # 37.5
+
+    # 位置計算
+    arts_left = img.width
+    arts_bottom = 38
+    arts_top = img.height - arts_bottom - art_height
+
+    # 背景（作品）描画
+    art_w = (img.width - 40 - 40 - (35 * 2)) / 3
     arts_rect = get_arts_rect(img)
     for i, art in enumerate(arts):
         art_img = Image.new("RGBA", img.size)
         art_draw = ImageDraw.Draw(art_img)
         art_h = arts_rect.height
-        art_x = 47 + (i * art_w) + (i * 20)
+        art_x = 40 + (i * art_w) + (i * 35)
         art_y = arts_rect.top
         art_draw.rectangle(
             (art_x, art_y, art_x + art_w, art_y + art_h),
             fill=(255, 255, 255, int(255 * 0.85)),
         )
+        # 描画
         draw_text_with_wrap(
             art_draw,
             (art_x + 30, art_y + 30),
@@ -54,61 +111,9 @@ def _draw_arts(
             font=ART_FONT,
         )
         img.alpha_composite(art_img)
- 
 
-def _draw_summary(
-    img: Image.Image,
-    username,
-    icon,
-    colors,
-):
-    draw = ImageDraw.Draw(img)
-    arts_rect = get_arts_rect(img)
-    summary_bottom = arts_rect.top - 20
-    summary_left = 100
+    pass
 
-    # サイズ計算
-
-    # アイコン
-    icon_w, icon_h = (260, 260)
-
-
-    # 名前
-    name_w = tags_w
-    name_bbox = textbbox_with_wrap(draw, (0, 0), name_w, username, font=NAME_FONT)
-    name_h = name_bbox[3] - name_bbox[1]
-
-    # 全体のサイズ
-    summary_h = rank_h + 16 + name_h + 27 + tag_h
-    summary_t = summary_bottom - summary_h
-
-    # 描画
-    # アイコン
-    icon_img = Image.open(icon)
-    icon_img = icon_img.resize((icon_w, icon_h))
-    icon_img = cut_rounded_rect(icon_img, 60)
-    img.alpha_composite(
-        icon_img,
-        dest=(
-            int(img.width - 100 - icon_w),
-            int(summary_t + (summary_h / 2 - icon_h / 2)),
-        ),
-    )  # TODO
-
-    # 名前
-    name_bottom = tags_rect.top - 27
-    name_top = name_bottom - name_h
-    name_left = summary_left
-    draw_text_with_wrap(
-        draw,
-        (name_left, name_top),
-        name_w,
-        username,
-        fill=colors.text.inner,
-        font=NAME_FONT,
-        stroke_width=5,
-        stroke_fill=colors.text.edge,
-    )
 
 def businesscard_type_4(
     username,
@@ -121,9 +126,18 @@ def businesscard_type_4(
 ):
     img = Image.open(background_image)
 
-    _draw_background_filter(img)
-    _draw_arts(img, arts, colors[theme_color])
-    _draw_summary(img, rank, username, interest_tags, icon, colors[theme_color])
+    _draw_icon_and_name(
+        img=img,
+        icon=icon,
+        username=username,
+        colors=colors[theme_color],
+    )
+
+    _draw_arts(
+        img=img,
+        arts=arts,
+        color=colors[theme_color],
+    )
 
     img.save("output.png")
 
